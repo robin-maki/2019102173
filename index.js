4c0f8df1736831668b11f563ca1f18428491fb06
robin* <robin_maki@planet.moe>
2020-10-16 11:34:45 +0900

    불쌍한 김베이가를 위해 8로 되돌리기

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-for(let i = 1; i <= 9; i++) { // TODO Implements 9 times table
+for(let i = 1; i <= 8; i++) { // TODO Implements 9 times table
     for(let j = 1; j <= 9; j++) {
         console.log(`${i} x ${j} = ${i * j}`);
     }
